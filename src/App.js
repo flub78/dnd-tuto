af25d3130e8da8294cf11626b9d8e65ace9e86ad
flub78 <frederic.peignot@free.fr>
2024-02-24 09:04:52 +0100

    Title change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Beautiful drag and drop tutorial
         </p>
         <a
           className="App-link"
