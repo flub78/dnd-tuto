f35e70fbf4ef6ba5aa8045c4d893ba202a57eef7
flub78 <frederic.peignot@free.fr>
2024-02-25 10:38:48 +0100

    warning fixes

--- src/Column.js
+++ src/Column.js
@@ -3,16 +3,15 @@ import styled from 'styled-components';
 import Task from './Task';
 import { Droppable } from 'react-beautiful-dnd';
 
-const Column = (props) => {
-
-    const Container = styled.div`
+const Container = styled.div`
         margin: 8px;
         border: 1px solid lightgrey;
         border-radius: 2px;`;
-    const Title = styled.h3`
+const Title = styled.h3`
         padding: 8px;`;
-    const TaskList = styled.div`
+const TaskList = styled.div`
         padding: 8px;`;
+const Column = (props) => {
 
     return (
         <Container>
